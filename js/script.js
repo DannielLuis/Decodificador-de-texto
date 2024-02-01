f16d80279499a2c96db69dacdf5cd5d84be9fafb
Daniel Luis <danniellcontato@gmail.com>
2024-02-01 15:10:31 -0300

    Encriptando texto

--- js/script.js
+++ js/script.js
@@ -31,18 +31,53 @@ const textValidate = () => {
     return validar
 }
 
-function encriptar(){
+function encriptar(string){
     let validar = textValidate();
+    let result = ''
+    let temp = []
     //console.log(typeof validar)
     //console.log(validar)
     if(!validar){
         alert("Não é possivel encriptar, texto invalido!")
+    }else if(validar){
+        for(let i = 0; i < string.length; i++){
+            //console.log(string.charAt(i))
+            temp[i] = string.charAt(i)
+        }
+
+        temp.forEach((element, index) => {
+            //console.log(index)
+            //console.log(element)
+            if(temp[index] == 'e'){
+                temp[index] = "enter"
+                //console.log(index)
+            }
+            if(temp[index] == 'i'){
+                temp[index] = "imes"
+            }
+            if(temp[index] == 'a'){
+                temp[index] = "ai"
+            }
+            if(temp[index] == 'o'){
+                temp[index] = "ober"
+            }
+            if(temp[index] == 'u'){
+                temp[index] = "ufat"
+            }
+        });
+    
+        for(let i = 0; i < temp.length; i++){
+            result = result += temp[i]
+        }
     }
+    return result
 }
 
 btn1.addEventListener("click", () => {
-    encriptar();
-})
+    let area = document.querySelector('.area_texto_input').value;
+    let result = encriptar(area);
+    console.log(result);
+});
 
 function testes(){
     alert('testando')
