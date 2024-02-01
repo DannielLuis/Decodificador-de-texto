9d6d5c3ab2507426934e8bbe5a034fefce7aa26f
Daniel Luis <danniellcontato@gmail.com>
2024-02-01 11:46:26 -0300

    Adição de borda vermelha na area de texto, se o texto for invalido

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 //
+const campoTexto = document.querySelectorAll('[campoTexto]');
 const maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const textRegExp = new RegExp(/[0-9]/)
 
 function textValidate(){
-    var area = document.querySelector('.area_texto_input').value
+    let area = document.querySelector('.area_texto_input').value
     for(i=0; i < area.length; i++){
         if(maiuscula.indexOf(area.charAt(i)) != -1){
             console.log("Somente letras minusculas!");
+            campoTexto[0].classList.add("text_invalid");
             //return
         }else if(textRegExp.test(area)){
             console.log("Texto invalido, não é permitido numeros!");
-        }/*else{
-            console.log(area)
-        }*/
+            campoTexto[0].classList.add("text_invalid");
+        }else{
+            //console.log(area)
+            campoTexto[0].classList.remove("text_invalid");
+        }
         //console.log(area.charAt(i))
     }
     //console.log(area)
