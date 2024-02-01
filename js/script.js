3cbc8828935f70947bbe4d85ba14548f22139ba4
Daniel Luis <danniellcontato@gmail.com>
2024-02-01 13:57:55 -0300

    Criação da function encriptar

--- js/script.js
+++ js/script.js
@@ -1,31 +1,48 @@
 //
 const campoTexto = document.querySelectorAll('[campoTexto]');
+const btn1 = document.querySelector(".btn1");
 const maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 //const textRegExp = new RegExp(/[0-9]/)
 const textRegExp = new RegExp(
     /[(0-9)(À-ù)(À-ü)('"!@)]/
 );
 
-function textValidate(){
+const textValidate = () => {
     let area = document.querySelector('.area_texto_input').value
+    let validar = true;
+
     for(i=0; i < area.length; i++){
         if(maiuscula.indexOf(area.charAt(i)) != -1){
             console.log("Somente letras minusculas!");
             campoTexto[0].classList.add("text_invalid");
+            validar = false
             //return
         }else if(textRegExp.test(area)){
             console.log("Texto invalido, não é permitido numeros!");
             campoTexto[0].classList.add("text_invalid");
-        }else{
+            validar = false
+        }else if(validar){
             //console.log(area)
             campoTexto[0].classList.remove("text_invalid");
         }
         //console.log(area.charAt(i))
     }
     //console.log(area)
+    return validar
 }
 
-textValidate();
+function encriptar(){
+    let validar = textValidate();
+    //console.log(typeof validar)
+    //console.log(validar)
+    if(!validar){
+        alert("Não é possivel encriptar, texto invalido!")
+    }
+}
+
+btn1.addEventListener("click", () => {
+    encriptar();
+})
 
 function testes(){
     alert('testando')
