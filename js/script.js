e9d7b4eafd93e128c548d0077727c2ec3775d1d4
Daniel Luis <danniellcontato@gmail.com>
2024-02-01 11:07:22 -0300

    Valida texto, procura por numeros

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 //
-let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const textRegExp = new RegExp(/[0-9]/)
 
 function textValidate(){
     var area = document.querySelector('.area_texto_input').value
     for(i=0; i < area.length; i++){
         if(maiuscula.indexOf(area.charAt(i)) != -1){
-            console.log("Somente letras minusculas!")
+            console.log("Somente letras minusculas!");
             //return
+        }else if(textRegExp.test(area)){
+            console.log("Texto invalido, não é permitido numeros!");
         }/*else{
             console.log(area)
         }*/
