c220063132a9b8de0af49a3ecbc0a0a23c8334dd
Daniel Luis <danniellcontato@gmail.com>
2024-02-01 08:54:57 -0300

    Verifica se o texto tem letras maiusculas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 //
-window.onload = function(){
-    alert("Testando js!!!")
+let maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+function textValidate(){
+    var area = document.querySelector('.area_texto_input').value
+    for(i=0; i < area.length; i++){
+        if(maiuscula.indexOf(area.charAt(i)) != -1){
+            console.log("Somente letras minusculas!")
+            //return
+        }/*else{
+            console.log(area)
+        }*/
+        //console.log(area.charAt(i))
+    }
+    //console.log(area)
 }
+
+textValidate();
+
+function testes(){
+    alert('testando')
+}
+
+
+
